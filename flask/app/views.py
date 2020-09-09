ce82de47dbba359547e532b30ec88df99751ce5a
Xarvius <strzelecki@gmail.com>
2020-09-09 16:43:06 +0200

    add bootstrap, env, some templates, basic form, configure docker (db wait), etc.

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,13 +1,22 @@
 from app import app
-import os
+from app.forms import LoginForms
+from markupsafe import escape
 
-@app.route("/")
-def index():
+from flask import url_for, render_template, redirect
 
-    # Use os.getenv("key") to get environment variables
-    app_name = os.getenv("APP_NAME")
 
-    if app_name:
-        return f"Hello from {app_name} running in a Docker container behind Nginx!"
+@app.route('/')
+def hello():
+    return render_template("basic.html")
 
-    return "Hello from Flask"
+
+@app.route('/test/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/login', methods=['POST', 'GET'])
+def forms_time():
+    form = LoginForms()
+    if form.validate_on_submit():
+        return redirect(url_for('show_user_profile', username=form.username))
+    return render_template("login.html", form=form)
--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,9 @@
 from app import app
+from config import Config
+from flask_bootstrap import Bootstrap
+
+app.config.from_object(Config)
+Bootstrap(app)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
